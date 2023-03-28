daf14429ab6315876dc92fad6203a4ffe8dcaeeb
Ganesh Babu <readingroomofgb@gmail.com>
2023-03-28 10:04:45 +0530

    Formatting changes in reduce.py

--- lambdas-and-collections/reduce.py
+++ lambdas-and-collections/reduce.py
@@ -1,7 +1,9 @@
 # Code to calculate sum of elements in a list
 
 from functools import reduce
-list_input=list(range(1, 5))
-the_sum = reduce(lambda acc,i: acc+i, list_input)  # accumulator initial value is not ddeclared. Hence, taking the first value in the list
-print(the_sum)
 
+list_input = list(range(1, 5))
+the_sum = reduce(lambda acc, i: acc + i, list_input)
+# accumulator initial value is not declared.
+# Hence, taking the first value in the list
+print(the_sum)
