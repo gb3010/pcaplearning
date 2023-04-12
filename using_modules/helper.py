1b2af6527e12dd46aba5a2e73384f6d45cbe603f
Ganesh Babu <readingroomofgb@gmail.com>
2023-04-12 10:04:29 +0530

    Made extract_lower a private function

--- using_modules/helper.py
+++ using_modules/helper.py
@@ -1,12 +1,15 @@
 # Defining functions to be called in from the module
 
 # Making only extract_upper callable while importing all
-__all__ = ['extract_upper']
+#__all__ = ['extract_upper']
+
+
 def extract_upper(name):
     return list(filter(str.isupper, name))
 
 
-def extract_lower(name):
+# Making extract_lower a private function which is not imported by default
+def _extract_lower(name):
     return list(filter(str.islower, name))
 
 
