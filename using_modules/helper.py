7ef1a096e6afb002c4ba8a5895ef3c1f9f333e98
Ganesh Babu <readingroomofgb@gmail.com>
2023-04-12 10:01:58 +0530

    added __all__ variable for entity hiding

--- using_modules/helper.py
+++ using_modules/helper.py
@@ -1,10 +1,14 @@
 # Defining functions to be called in from the module
+
+# Making only extract_upper callable while importing all
+__all__ = ['extract_upper']
 def extract_upper(name):
     return list(filter(str.isupper, name))
 
 
 def extract_lower(name):
     return list(filter(str.islower, name))
+
 
 if __name__ == "__main__":
     print("Hello from Helper")
