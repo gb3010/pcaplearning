df11b8427240535785cc0bc26effae49f2f9e512
Ganesh Babu <readingroomofgb@gmail.com>
2023-05-16 14:05:33 +0530

    Added code to func as parameter

--- class-and-objects/decorators/first_class_functions.py
+++ class-and-objects/decorators/first_class_functions.py
@@ -12,3 +12,18 @@ def whisper(word):
 yell = shout  # Here we have assigned the 'shout' function to the variable named 'yell'
 print(yell('Hello'))
 
+# Pass a function as an argument to another function
+
+def double(x):
+    return x * 2
+
+def inc(x):
+    return x + 1
+
+def func_input(func, x):
+    output = func(x)
+    print(output)
+
+func_input(double(2))
+func_input(inc(2))
+
